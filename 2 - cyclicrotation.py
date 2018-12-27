ad2955018aa17670209762b96b9c3bc698e3d1ef
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-12-27 09:37:28 -0200

    2 - Cycle Rotation [Empty Array]

--- 2 - cyclicrotation.py
+++ 2 - cyclicrotation.py
@@ -1,9 +1,9 @@
 def cycleRotation(array, rotations):
-    for rotation in range(rotations):
-        last_element = array[-1]
-        del array[-1]
-        array = [last_element] + array
-
+    if len(array) > 0:
+        for rotation in range(rotations):
+            last_element = array[-1]
+            del array[-1]
+            array = [last_element] + array
     return array
 
-cycleRotation([3, 8, 9, 7, 6], 3)
+cycleRotation([], 3)
